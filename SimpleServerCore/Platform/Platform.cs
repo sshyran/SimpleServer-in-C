3c66208e459073c5940055d7f11271a99e23fd46
DMP9Labs <dylanperks23@gmail.com>
2017-01-28 17:36:22 +0000

    SimpleServer R Initial Commit

---
+++ SimpleServerCore/Platform/Platform.cs
@@ -0,0 +1,15 @@
+﻿using SimpleServer.Core.Platform.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServer.Core.Platform
+{
+    public interface Platform
+    {
+        WebRequest CreateWebRequest(string url);
+
+    }
+}
---
+++ SimpleServerNET/DotNetPlatform.cs
@@ -0,0 +1,18 @@
+﻿using SimpleServer.Core.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleServer.Core.Platform.Net;
+
+namespace SimpleServer.DotNet
+{
+    public class DotNetPlatform : Platform
+    {
+        public WebRequest CreateWebRequest(string url)
+        {
+            return (WebRequest)new DotNetWebRequest(url);
+        }
+    }
+}
