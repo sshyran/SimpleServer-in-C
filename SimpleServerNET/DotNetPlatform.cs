e7818851e9228e32f46ec8bbdf40904025a26190
DMP9Labs <dylanperks23@gmail.com>
2017-01-28 20:26:21 +0000

    .NET Framework 3.5

--- SimpleServerNET/DotNetPlatform.cs
+++ SimpleServerNET/DotNetPlatform.cs
@@ -5,14 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SimpleServer.Core.Platform.Net;
+using SimpleServer.Core.Platform.Net.Listeners;
 
 namespace SimpleServer.DotNet
 {
     public class DotNetPlatform : Platform
     {
+        public HttpListener CreateHttpListener(Listener underlyingListener)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Listener CreateListener(int port)
+        {
+            throw new NotImplementedException();
+        }
+
         public WebRequest CreateWebRequest(string url)
         {
             return (WebRequest)new DotNetWebRequest(url);
+        }
+
+        public WebServer CreateWebServer(int port)
+        {
+            throw new NotImplementedException();
         }
     }
 }
