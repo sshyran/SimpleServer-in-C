14b0a5d8463cac0de89a28d1f702b9a8a309209f
DMP9 <dylanperks23@gmail.com>
2017-04-23 20:19:02 +0100

    3rd recode, let's do this
    Completely ditched the last two versions. Reasons:
    
    - We're using .NET Core for the server work because it runs everywhere -- no need for all those implementations
    - The old was just messyyyyyy

---
+++ sss/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
