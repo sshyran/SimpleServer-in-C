568203b8666447dd16a4f5bd45c7f92b7edfd7b3
DMP9 <dylanperks23@gmail.com>
2017-10-01 10:55:06 +0100

    Start http

---
+++ SimpleServer/IRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleServer
+{
+    public interface IRequestHandler
+    {
+        string[] GetEndpoints();
+        void Handle();
+    }
+}
---
+++ SimpleServer/ServerRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleServer
+{
+    public class ServerRequest
+    {
+        internal NetworkStream stream;
+        internal ServerRequest(NetworkStream s)
+        {
+            stream = s;
+            StreamReader sr = new StreamReader(s,Encoding.UTF8,false,4096,true);
+            string line;
+            List<string> lines = new List<string>();
+            while (string.IsNullOrEmpty(line = sr.ReadLine()))
+            {
+                lines.Add(line);
+            }
+        }
+        public string Method { get; }
+        public string Version { get; }
+        public string Url { get; }
+        public Dictionary<string, string> Headers { get; }
+        public Stream Body { get; }
+    }
+}
